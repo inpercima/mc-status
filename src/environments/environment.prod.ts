452f1055a46faadd4784ae811d96083ec9c56e4d
inpercima <inpercima@gmail.com>
2022-01-15 13:53:33 +0100

    Add production config

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  api: './',
+  appname: 'Minecraft server status',
+  defaultRoute: 'dashboard',
+  production: false,
+  theme: 'indigo-pink',
+  serverIp: '85.214.57.99',
+  serverPort: '25565',
+};
