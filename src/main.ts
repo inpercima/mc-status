8535e33a27d017122630181bada1be773c146568
Marcel Jänicke <inpercima@gmail.com>
2023-01-20 11:02:03 +0100

    Update to standalone components

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { provideHttpClient } from '@angular/common/http';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
-import { AppModule } from './app/app.module';
+import { AppComponent } from './app/app.component';
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [provideAnimations(), provideHttpClient()],
+}).catch((err) => console.error(err));
